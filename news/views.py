de1b51dcd4fe988b801df1b35a22c2977c346539
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-01 12:09:40 +0300

    added new article views

--- news/views.py
+++ news/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article,NewsLetterRecipients
+from .forms import  NewsLetterForm, NewArticleForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
@@ -10,18 +13,22 @@ def welcome(request):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
 
-# def convert_dates(dates):
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
 
-#     # Function that gets the weekday number for the date.
-#     day_number = dt.date.weekday(dates)
+            HttpResponseRedirect('news_today')
+            #.................
+    else:
+            form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
-#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-#     # Returning the actual day of the week
-#     day = days[day_number]
-#     return day
 
 def past_days_news(request, past_date):
     try:
@@ -57,3 +64,18 @@ def article(request,article_id):
     except Article.DoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
